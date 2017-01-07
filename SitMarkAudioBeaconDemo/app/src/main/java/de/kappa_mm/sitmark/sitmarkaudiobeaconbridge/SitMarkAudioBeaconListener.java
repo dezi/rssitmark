5a9b05e4e99bf52670b1a42dd836b1f6140e9f1b
dezi <dezi@kappa-mm.de>
2017-01-07 10:47:46 +0100

    Check in.

---
+++ SitMarkAudioBeaconDemo/app/src/main/java/de/kappa_mm/sitmark/sitmarkaudiobeaconbridge/SitMarkAudioBeaconListener.java
@@ -0,0 +1,18 @@
+package de.kappa_mm.sitmark.sitmarkaudiobeaconbridge;
+
+public class SitMarkAudioBeaconListener
+{
+    private static final String LOGTAG = SitMarkAudioBeaconListener.class.getSimpleName();
+
+    private boolean isListening;
+
+    public void onStartListening()
+    {
+
+    }
+
+    public void onStopListening()
+    {
+
+    }
+}
--- SitMarkAudioBeaconDemo/app/src/main/java/de/kappa_mm/sitmark/sitmarkaudiobeacondemo/MainActivity.java
+++ SitMarkAudioBeaconDemo/app/src/main/java/de/kappa_mm/sitmark/sitmarkaudiobeacondemo/MainActivity.java
@@ -1,14 +1,23 @@
 package de.kappa_mm.sitmark.sitmarkaudiobeacondemo;
 
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ToggleButton;
 import android.widget.TextView;
 import android.graphics.Color;
+import android.view.View;
 import android.os.Bundle;
+import android.util.Log;
 
 import de.kappa_mm.sitmark.sitmarkaudiobeaconbridge.SitMarkAudioBeaconBridge;
+import de.kappa_mm.sitmark.sitmarkaudiobeaconbridge.SitMarkAudioBeaconListener;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final String LOGTAG = MainActivity.class.getSimpleName();
+    private static final String VERSION = "07.01.2017:0";
+
+    private SitMarkAudioBeaconListener listener;
+
     static
     {
         //
@@ -25,6 +34,8 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTitle("RS - Demo - " + VERSION);
+
         //
         // Get version string and check for desired version.
         //
@@ -32,8 +43,40 @@ public class MainActivity extends AppCompatActivity
         String version = SitMarkAudioBeaconBridge.getVersionString();
         boolean versionok = SitMarkAudioBeaconBridge.isDesiredVersion();
 
-        TextView tv = (TextView) findViewById(R.id.sample_text);
+        TextView tv = (TextView) findViewById(R.id.version);
         tv.setBackgroundColor(versionok ? Color.GREEN : Color.RED);
         tv.setText(version);
+
+        //
+        // Setup beacon listener.
+        //
+
+        listener = new SitMarkAudioBeaconListener();
+
+        //
+        // Setup start/stop button.
+        //
+
+        ToggleButton tb = (ToggleButton) findViewById(R.id.toggleButton);
+
+        tb.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                boolean on = ((ToggleButton) view).isChecked();
+
+                Log.d(LOGTAG, "toggleButton=" + on);
+
+                if (on)
+                {
+                    listener.onStartListening();
+                }
+                else
+                {
+                    listener.onStopListening();
+                }
+            }
+        });
     }
 }
